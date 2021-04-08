394e659dbdeb1ec7e439ca78a5cca4316fd055d4
Will Lotherington <will.lotherington@hey.com>
2021-04-08 18:33:47 -0500

    initial commit

---
+++ rpl-server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+
+const http = require("http").Server(app);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("rpl-server");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("disconnect", () => {
+    console.log("a user disconnected");
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Server started on port ${port}. press Ctrl + C to terminate`);
+});
