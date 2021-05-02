7c404a4677e592873df8697ebb6b5a1206076756
Will Lotherington <will.lotherington@hey.com>
2021-05-02 13:43:08 -0500

    Add redis package

--- ekko.js
+++ ekko.js
@@ -3,7 +3,8 @@ const app = require("express")();
 const server = require("http").Server(app);
 const cors = require("cors");
 const socketio = require("socket.io");
-const redis = require("socket.io-redis");
+const socketioRedis = require("socket.io-redis");
+const redis = require("redis");
 const express = require("express");
 
 const port = process.env.PORT || 3000;
@@ -11,7 +12,7 @@ const redisHost = process.env.REDIS_ENDPOINT || "localhost";
 const redisPort = process.env.REDIS_PORT || 6379;
 
 const io = socketio(server, { cors: { origin: "*" } });
-io.adapter(redis({ host: redisHost, port: redisPort }));
+io.adapter(socketioRedis({ host: redisHost, port: redisPort }));
 const ekkoApps = io.of(/.*/);
 
 // Managers2
@@ -66,77 +67,31 @@ ekkoApps.on("connection", (socket) => {
   socket.on("publish", (params) => handlePublish(socket, params));
 });
 
+// Update Associations
+const redisSubscriber = redis.createClient(redisPort, redisHost);
+const redisPublisher = redis.createClient(redisPort, redisHost);
+redisSubscriber.subscribe("ekko-associations");
+redisSubscriber.on("message", (channel, message) => {
+  // UPDATE IN MEMORY ASSOCIATION STUFF
+  console.log(message);
+});
+
 // TODO: !!! CAN ADD IF WE WANT TO BE ABLE TO SEE THAT SERVER IS RUNNING
 app.get("/", (req, res) => {
   res.send("ekko-server"); // TODO: Should this endpoint render anything?
 });
 
 app.put("/associations", (req, res) => {
-  //send req.body to associations manager
-  //decrypt
-  // console.log("request ", req.body);
-  // const updatedAssociations = handleAssociationsDecoding(req.body.token);
-  // if (updatedAssociations) {
-  // console.log("SENDING");
-  // redisEmitterIo
-  //   .of("helloworld")
-  //   .to("greeting")
-  //   .emit("publish", {
-  //     channel: "greeting",
-  //     message: { text: "YAYAYA" },
-  //   });
-  // console.log("SENT");
-  // res.sendStatus(200);
-  // } else {
-  // res.status(400).send("Invalid JWT");
-  // }
+  const updatedAssociations = handleAssociationsDecoding(req.body.token);
+  if (updatedAssociations) {
+    console.log(updatedAssociations);
+    redisPublisher.publish("ekko-associations", "HELLO");
+    res.sendStatus(200);
+  } else {
+    res.status(400).send("Invalid JWT");
+  }
+  res.end();
 });
-
-// ekko client
-// const Ekko = require("ekko-realtime-client");
-// const jsonWebToken = require("jsonwebtoken");
-// const { json } = require("express");
-// const secret = process.env.SECRET_KEY || "SECRET";
-// const appName = "ekko";
-// const jwt = jsonWebToken.sign({ appName, admin: false }, secret);
-
-// const ekko = new Ekko({
-//   host: "http://localhost:3000/",
-//   jwt,
-//   appName,
-// });
-
-// ekko.subscribe({ channels: ["associations"] });
-// ekko.addListener({
-//   message: (ekkoEvent) => {
-//     const associations = jsonWebToken.verify(ekkoEvent.token, secret);
-//     console.log(associations);
-//   },
-// });
-
-// setInterval(() => {
-//   const associations = {
-//     applications: {
-//       app_1: {
-//         channels: [
-//           { channelName: "channel_1", functionNames: ["capitalize"] },
-//           { channelName: "channel_2", functionNames: ["reverse"] },
-//           { channelName: "channel_3", functionNames: ["emphasize"] },
-//           {
-//             channelName: "channel_4",
-//             functionNames: ["capitalize", "reverse", "emphasize"],
-//           },
-//         ],
-//       },
-//     },
-//   };
-
-//   const token = jsonWebToken.sign(associations, secret);
-//   ekko.publish({
-//     channel: "associations",
-//     token,
-//   });
-// }, 2000);
 
 server.listen(port, () => {
   const message = `Server: ekko server started on port ${port}`;
